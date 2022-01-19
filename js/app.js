8ce18deca1767c878e7f8dd74b906f8fc5ffb59a
rem765321 <885317429@qq.com>
2022-01-19 15:28:10 +0800

    Site updated: 2022-01-19 15:28:10

--- js/app.js
+++ js/app.js
@@ -1,21 +1,33 @@
+// 表格相册
 $("table")
   .has("img")
   .addClass("nexmoe-album");
 
-$("#nexmoe-content img").each(function() {
-  $(this).attr("data-src", $(this).attr("src"));
-  $(this).attr("src", "");
-  $(this).addClass("lazyload");
-  $(this).attr("referrerPolicy", "no-referrer");
+// 搜索
+function search() {
+  window.open($("#search_form").attr("action_e") + " " + $("#search_value").val());
+  return false;
+}
+
+// 平滑跳转同时修复锚点链接被转义
+$(document).ready(function () {
+  $("a.toc-link").click(function (ev) {
+    ev.preventDefault();
+    $("html, body").animate({
+      scrollTop: $(decodeURI($(this).attr("href"))).offset().top - 25
+    }, {
+      duration: 500,
+      easing: "swing"
+    });
+  });
 });
 
-$("article:not(.nexmoe-py) img").each(function() {
-  var element = document.createElement("a");
-  $(element).attr("data-fancybox", "gallery");
-  $(element).attr("href", $(this).attr("data-src"));
-  $(element).attr("title", $(this).attr("alt"));
-  $(this).wrap(element);
+$(document).on("copy", function(){
+  if(!window.copyTip){ return; }
+  var sel = document.getSelection();
+  var ele = document.createElement("div");
+  ele.innerHTML = '<div style="position: fixed;opacity: 0;white-space: pre;">' + sel + "\n\n" + window.copyTip.replaceAll("%url",document.location.href) + ' </div>' 
+  document.body.appendChild(ele);
+  sel.selectAllChildren(ele);
+  setTimeout(function () {document.body.removeChild(ele);});
 });
-
-$("#nexmoe-sidebar a").addClass("mdui-ripple");
-mdui.mutation();
